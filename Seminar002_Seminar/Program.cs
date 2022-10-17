b92204ec96df71aa50e82e95730aeb4e1b675d0f
Ilya <efimov4242@mail.ru>
2022-10-17 23:45:57 +0400

    Семинар 2

---
+++ Seminar002_Seminar/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру.
+
+int MaxDigit(int number)
+{
+	{
+	if(number % 10 > number / 10)
+	return number % 10; // Остаток от деления на 10
+	}
+	return number / 10; // Число без остатка
+}
+int number = new Random().Next(10, 100);
+System.Console.WriteLine(MaxDigit(number));
