d2463406a63a9acd74914b1e032651197479dc4a
Ilya <efimov4242@gmail.com>
2022-10-18 10:47:35 +0300

    Задача №2

--- Seminar002_HomeWork02/Program.cs
+++ Seminar002_HomeWork02/Program.cs
@@ -4,9 +4,26 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-int GetLastDigit(int number)
+int LastDigit(int number)
 {
-    if(number )
+    {
+	if(number > 99 & number < 999)
+	return number % 10;
+    }
+    {
+	if(number > 999 & number < 9999)
+	return number / 10 % 10;
+    }
+    {
+    if(number > 9999 & number < 99999)
+	return number / 100 % 10;
+    }
+    {
+    return -1;
+    }
 }
 
-System.Console.WriteLine(GetLastDigit(258));
+System.Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(LastDigit(number));
