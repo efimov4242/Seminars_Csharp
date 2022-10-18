0d5b6d2ab89ae5df8c61a5ca7245a9699421da37
Ilya <efimov4242@gmail.com>
2022-10-18 16:09:55 +0300

    Задача №4

---
+++ Seminar002_HomeWork04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дополнительная Задача 9
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+// void GetCheckNumber(int number)
+// {
+// 	{
+// 	if(number % 7 == 0 & number % 23 == 0)
+// 	System.Console.WriteLine("Да");
+// 	}
+// 	System.Console.WriteLine("Нет");
+// }
+
+Console.WriteLine("Введите число кратное одновременно 7 и 23");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 7 == 0 & number % 23 == 0)
+	System.Console.WriteLine("Да");
+else
+	System.Console.WriteLine("Нет");
+
