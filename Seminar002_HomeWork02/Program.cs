7a9d9c89cd97812512dc62d6a8cab8b9f531d95e
Ilya <efimov4242@gmail.com>
2022-10-19 10:53:24 +0300

    Решение преподавателя

--- Seminar002_HomeWork02/Program.cs
+++ Seminar002_HomeWork02/Program.cs
@@ -8,19 +8,32 @@ Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
 
 if (number < 100)
-	Console.WriteLine("Третьей цифры нет");
-if (number >= 100 & number <= 1000)
-	Console.WriteLine(number % 10);
+{
+Console.WriteLine("Третьей цифры нет");
+}
 else
 {
-	int index = 100;
-	while (index < number)
-    {
-        number = number / 10;
-        if(number > 100 & number < 1000)
-            Console.WriteLine(number % 10);
-        index++;
-    }
+while (number >= 1000)
+{
+number /= 10;
 }
+Console.WriteLine(number % 10);
+}
+
+// if (number < 100)
+// 	Console.WriteLine("Третьей цифры нет");
+// if (number >= 100 & number <= 1000)
+// 	Console.WriteLine(number % 10);
+// else
+// {
+// 	int index = 100;
+// 	while (index < number)
+//     {
+//         number = number / 10;
+//         if(number > 100 & number < 1000)
+//             Console.WriteLine(number % 10);
+//         index++;
+//     }
+// }
 
 
