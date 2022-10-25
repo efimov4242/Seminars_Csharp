7f7ff43719f01a1e97686a80aab156e40fca1b58
Ilya <efimov4242@gmail.com>
2022-10-25 15:38:36 +0400

    Задача 1

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, котора принимает число и выдает количество цифр в числе
+int PrintCountDigits(int number)
+{
+	return number.ToString().Length;
+}
+
+int PrintCountDigitsFor(int number)
+{
+	int count = 0;
+	for (int i = 1; i <= number; i++)
+	{
+		number = number / 10;
+		count = count + 1;
+	}
+	return count;
+}
+
+Console.WriteLine(PrintCountDigits(12));
+Console.WriteLine(PrintCountDigitsFor(12));
+
