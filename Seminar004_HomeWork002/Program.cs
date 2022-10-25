dae70a0181caa539f2577ac4ae93d1fc1c9f159f
Ilya <efimov4242@gmail.com>
2022-10-25 17:14:58 +0400

    Задача 2

---
+++ Seminar004_HomeWork002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSumDigits(int number)
+{
+	int i = 0;
+	int sum = 0;
+	while (i < number)
+	{
+		sum += number % 10;
+		number /= 10;
+	}
+	return sum;
+}
+
+Console.WriteLine(GetSumDigits(452)); // -> 11
