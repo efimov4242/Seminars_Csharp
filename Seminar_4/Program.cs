99c2210cba26572a5fbbeea18b833b782de034b3
Ilya <efimov4242@gmail.com>
2022-10-25 16:29:17 +0400

    Добавлена Задача 2

--- Seminar_4/Program.cs
+++ Seminar_4/Program.cs
@@ -1,4 +1,5 @@
-﻿// Напишите программу, котора принимает число и выдает количество цифр в числе
+﻿// Задача 1
+// Напишите программу, котора принимает число и выдает количество цифр в числе
 int PrintCountDigits(int number)
 {
 	return number.ToString().Length;
@@ -15,6 +16,21 @@ int PrintCountDigitsFor(int number)
 	return count;
 }
 
-Console.WriteLine(PrintCountDigits(12));
-Console.WriteLine(PrintCountDigitsFor(12));
+// Console.WriteLine(PrintCountDigits(12));
+// Console.WriteLine(PrintCountDigitsFor(12));
+
+// Задача 2
+// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int FactorialNumber(int number)
+{
+	int count = 1;
+	for (int i = 1; i <= number; i++)
+	{
+		count = count * i;
+	}
+	return count;
+}
+
+System.Console.WriteLine(FactorialNumber(5));
 
