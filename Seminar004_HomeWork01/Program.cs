2e54c37501ab17764f1778c14a0175ffaeac9d6a
Ilya <efimov4242@gmail.com>
2022-10-25 17:26:13 +0400

    Начало решения Задачи №1

---
+++ Seminar004_HomeWork01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNaturalDegreeNumber(int number, int pow)
+{
+	int count = 0;
+	for (int i = 0; i <= pow; i++)
+	{
+		count = number * i;
+	}
+	return count;
+}
+
+System.Console.WriteLine(GetNaturalDegreeNumber(3, 5));
