a177f5869bcd7224217120f85847bd826cf1f7bf
Ilya <efimov4242@gmail.com>
2022-10-28 12:56:58 +0400

    PrintArrayRandom8

--- Seminar004_HomeWork003v2/Program.cs
+++ Seminar004_HomeWork003v2/Program.cs
@@ -1,39 +1,9 @@
-﻿void Array()
+﻿int[] array = new int[8];
+Random random = new Random();
+Console.Write("Массив: ");
+for (int i = 0; i < array.Length; i++)
 {
-
-int a = new Random().Next(0, 100);
-
-int b = new Random().Next(0, 100);
-
-int c = new Random().Next(0, 100);
-
-int d = new Random().Next(0, 100);
-
-int e = new Random().Next(0, 100);
-
-int f = new Random().Next(0, 100);
-
-int g = new Random().Next(0, 100);
-
-int h = new Random().Next(0, 100);
-
-int[] myArray = {a, b, c, d, e, f, g, h};
-
-Console.Write("[");
-
-for (int i = 0; i < myArray.Length; i++)
-{
-	Console.Write(myArray[i]);
-	if(i < myArray.Length -1)
-	{
-		for (int j = 0; j < 1; j++)
-		{
-		Console.Write(", ");
-		}
-	}
+	array[i] = random.Next(100);
+	Console.Write("{0} ", array[i]);
 }
-Console.Write("]");
 Console.WriteLine();
-}
-
-Array();
