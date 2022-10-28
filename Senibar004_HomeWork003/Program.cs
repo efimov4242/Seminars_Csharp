aadf1f917daa4f7db5401b645276577afd16078f
Ilya <efimov4242@gmail.com>
2022-10-28 09:51:55 +0400

    Задача 3

--- Senibar004_HomeWork003/Program.cs
+++ Senibar004_HomeWork003/Program.cs
@@ -2,14 +2,34 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] PrintArray()
+Console.WriteLine("Введите 8 чисел");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = int.Parse(Console.ReadLine());
+
+int c = int.Parse(Console.ReadLine());
+
+int d = int.Parse(Console.ReadLine());
+
+int e = int.Parse(Console.ReadLine());
+
+int f = int.Parse(Console.ReadLine());
+
+int g = int.Parse(Console.ReadLine());
+
+int h = int.Parse(Console.ReadLine());
+
+int[] myArray = {a, b, c, d, e, f, g, h};
+
+for (int i = 0; i < myArray.Length; i++)
 {
-	int n = 8; // колличество элементов в массиве
-	int[] array = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		array[i] = new Random().Next(0, 100);
-		Console.Write($"{array[i]}");
-	}
-	return array;
+	Console.Write(myArray[i]);
+	if(i < myArray.Length -1)
+		for (int j = 0; j < 1; j++)
+		{
+		Console.Write(", ");
+		}
 }
+
+Console.WriteLine();
