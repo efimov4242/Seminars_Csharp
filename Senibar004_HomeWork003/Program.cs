a74f5678af5afaede042908531a9c0e8578e3e62
Ilya <efimov4242@gmail.com>
2022-10-28 14:13:47 +0400

    Задача №3 с параметрами

--- Senibar004_HomeWork003/Program.cs
+++ Senibar004_HomeWork003/Program.cs
@@ -2,34 +2,25 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine("Введите 8 чисел");
 
-int a = int.Parse(Console.ReadLine());
-
-int b = int.Parse(Console.ReadLine());
-
-int c = int.Parse(Console.ReadLine());
-
-int d = int.Parse(Console.ReadLine());
-
-int e = int.Parse(Console.ReadLine());
-
-int f = int.Parse(Console.ReadLine());
-
-int g = int.Parse(Console.ReadLine());
-
-int h = int.Parse(Console.ReadLine());
-
-int[] myArray = {a, b, c, d, e, f, g, h};
-
-for (int i = 0; i < myArray.Length; i++)
+void PrintArray(int indexCount, int maxValue)
 {
-	Console.Write(myArray[i]);
-	if(i < myArray.Length -1)
-		for (int j = 0; j < 1; j++)
+
+int[] array = new int[indexCount];
+Random rand = new Random();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+	array[i] = rand.Next(maxValue);
+	Console.Write(array[i]);
+	if(i < array.Length -1)
 		{
 		Console.Write(", ");
 		}
 }
-
+Console.Write("]");
 Console.WriteLine();
+}
+
+PrintArray(50, 10);
+
