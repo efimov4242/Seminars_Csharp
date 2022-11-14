a92e12bf34aa9d81728d135872d392493e2609db
Ilya <efimov4242@gmail.com>
2022-11-14 12:04:41 +0400

    Файл gitignore

--- Senibar004_HomeWork003/Program.cs
+++ Senibar004_HomeWork003/Program.cs
@@ -3,7 +3,7 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
-void PrintArray(int indexCount, int maxValue)
+/*void PrintArray(int indexCount, int maxValue)
 {
 
 int[] array = new int[indexCount];
@@ -23,4 +23,39 @@ Console.WriteLine();
 }
 
 PrintArray(50, 10);
+*/
+/*
+int[] array = {2, 1, 3, 5, 7, 8};
+	for (int j = 0; j < array.Length; j++)
+	{
+		for (int i = 0; i < array.Length-1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+			int temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			}
+
+		}
+		Console.Write("{0} ", array[j]);
+	}
+*/
+
+int PrintFactorial(int number)
+{
+	int result = 1;
+	for (int i = 1; i <= number; i++)
+	{
+		result = result * i;
+
+	}
+	return result;
+}
+
+System.Console.WriteLine(PrintFactorial(4));
+
+
+
+
 
