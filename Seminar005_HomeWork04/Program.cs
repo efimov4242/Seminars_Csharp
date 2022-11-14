b4ed67fe1edc5d07393e6244fdcf5e49eb8b95d1
Ilya <efimov4242@gmail.com>
2022-11-14 15:28:16 +0400

    Дополнительная задача

---
+++ Seminar005_HomeWork04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дополнительно. Дан массив чисел. Напишите функцию, которая ищет заданное число в массиве и показывает нужный индекс в нем. При отсутствии числа возвращать -1
+
+int[] arr = { 1, 5, 3, 4, 6 };
+
+int GetNumbIndex(int[] array, int number)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if(array[i] == number)
+			return i;
+	}
+	return -1;
+}
+
+System.Console.WriteLine(GetNumbIndex(arr, 5));
