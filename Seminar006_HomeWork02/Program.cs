a91526a0699d9e04823f93b3be038c09b1afa7da
efimov4242 <64278427+efimov4242@users.noreply.github.com>
2022-11-15 23:33:24 +0400

    Решение с дополнением

--- Seminar006_HomeWork02/Program.cs
+++ Seminar006_HomeWork02/Program.cs
@@ -4,17 +4,16 @@
 
 int PrintPointOfIntersection(double b1, double k1, double b2, double k2)
 {
-	
-	if((k1 == k2) && (b1 == b2)) Console.WriteLine("Прямые совпадают");
+	if ((k1 == k2) && (b1 == b2)) Console.WriteLine("Прямые совпадают");
 
-	else if(k1 == k2) Console.WriteLine("Прямые параллельны");
+	else if (k1 == k2) Console.WriteLine("Прямые параллельны");
 
 	else
 	{
-		double x = (b2-b1) / (k1-k2);
+		double x = (b2 - b1) / (k1 - k2);
 		double y = k1 * x + b1;
 	}
 	return 0;
 }
 
-System.Console.WriteLine(PrintPointOfIntersection(10,10,10,10));
+System.Console.WriteLine(PrintPointOfIntersection(10, 10, 10, 10));
