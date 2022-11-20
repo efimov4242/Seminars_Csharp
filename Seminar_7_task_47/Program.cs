3685ebdcc8a463b268beedb50e0d057c68d499b3
Ilya <efimov4242@mail.ru>
2022-11-20 23:11:54 +0400

    Задача 52

---
+++ Seminar_7_task_47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double[,] RandomArray(int m = 3, int n = 4, int minValue = -10, int maxValue = 10)
+{
+	double[,] newArray = new double[m, n];
+	var rnd = new Random();
+	for (int i = 0; i < newArray.GetLength(0); i++)
+	{
+		for (int j = 0; j < newArray.GetLength(1); j++)
+		{
+			newArray[i, j] = Math.Round(rnd.NextDouble() * (maxValue - minValue) + minValue, 1);
+		}
+	}
+	return newArray;
+}
+
+
+void PrintArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write(array[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+double[,] randomArray = RandomArray();
+
+PrintArray(randomArray);
---
+++ Seminar_7_task_52/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void GetAverageColumn(int[,] array)
+{
+	Console.Write("Среднее арифметическое каждого столбца: ");
+	for (int i = 0; i < array.GetLength(1); i++)
+	{
+		double colSum = 0;
+		double average = 0;
+		for (int j = 0; j < array.GetLength(0); j++)
+		{
+			colSum += array[j, i];
+		}
+		average = colSum / array.GetLength(0);
+		Console.Write(Math.Round(average, 1) + " ");
+	}
+
+}
+
+
+int[,] someArray = new int[,] {{1, 4, 7, 2},
+										{5, 9, 2, 3},
+										{8, 4, 2, 4}};
+
+GetAverageColumn(someArray);
